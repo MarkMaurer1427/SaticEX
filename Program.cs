d7b646d13179be26c4d962b9839b2eeefc2f6e59
Mark Maurer <markmaurer1427@gmail.com>
2021-03-29 20:30:55 -0500

    new repo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The boiling point of water is {Math.Floor(TempConverter.CelsiusToFahrenheit(100.0))} degrees fahrenheit.");
+            Console.WriteLine($"In degrees celsius it is {Math.Floor(TempConverter.FahrenheitToCelsius(212.0))}.");
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticEX
+{
+    static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double input)
+        {
+            double celsius = input;
+            celsius -= 32;
+            celsius *= 0.56;
+            return celsius;            
+        }
+
+        public static double CelsiusToFahrenheit(double input)
+        {
+            double fahrenheit = input;
+            fahrenheit *= 1.8;
+            fahrenheit += 32;
+            return fahrenheit;
+        }
+    }
+}
